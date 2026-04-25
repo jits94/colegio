77216507252bd808e9c464251287b24057a67019
jitsDev94 <jmoron@grt.technology>
2026-04-24 22:05:27 -0400

    Update favicon to logoInnova3.png and clean up extensiones.php

--- contenido/extensiones.php
+++ contenido/extensiones.php
@@ -1,30 +1,28 @@
-
 <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Colegio</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
-  <!-- Favicons -->
-  <link href="../../assets/img/Logo_Innova.png" rel="icon">
-  <link href="../../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+<title>Colegio</title>
+<meta content="" name="description">
+<meta content="" name="keywords">
+<!-- Favicons -->
+<link href="../../imagenes/logoInnova3.png" rel="icon">
+<link href="../../imagenes/logoInnova3.png" rel="apple-touch-icon">
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+<!-- Google Fonts -->
+<link href="https://fonts.gstatic.com" rel="preconnect">
+<link
+  href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+  rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="../../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="../../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="../../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="../../assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="../../assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="../../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="../../assets/vendor/simple-datatables/style.css" rel="stylesheet">
-  <!-- <script src="assets/vendor/css/lightbox.css"></script> -->
-  <!-- <link href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-dark@4/dark.css" rel="stylesheet"> -->
-    <!-- fullCalendar -->
-    <link rel="stylesheet" href="../../assets/vendor/fullcalendar/main.css">
-
-  
-
+<!-- Vendor CSS Files -->
+<link href="../../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="../../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link href="../../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<link href="../../assets/vendor/quill/quill.snow.css" rel="stylesheet">
+<link href="../../assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+<link href="../../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+<link href="../../assets/vendor/simple-datatables/style.css" rel="stylesheet">
+<!-- <script src="assets/vendor/css/lightbox.css"></script> -->
+<!-- <link href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-dark@4/dark.css" rel="stylesheet"> -->
+<!-- fullCalendar -->
+<link rel="stylesheet" href="../../assets/vendor/fullcalendar/main.css">
